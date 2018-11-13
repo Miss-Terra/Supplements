48c29d1008e89f8f1c839b5538e0de53b1c00e2e
Terra Hunter <ms.terra.h@gmail.com>
2018-11-12 16:10:10 -0800

    Added references

--- src/com/mycompany/myapp/info/Info.java
+++ src/com/mycompany/myapp/info/Info.java
@@ -1,6 +1,10 @@
 package com.mycompany.myapp.info;
 
 public class Info {
+	
+	private String field;
+	private String[] reference;
+	
 	public Info() {
 		// TODO Auto-generated constructor stub
 	}
